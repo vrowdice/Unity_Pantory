9ac02086aa09fd52a3065bd7da62bae39d0bb67b
Min Jea Kim <minmin310342@gmail.com>
2026-04-12 18:00:03 +0900

    0412

--- Assets/Scripts/Runner/RunnerBase.cs
+++ Assets/Scripts/Runner/RunnerBase.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class RunnerBase : MonoBehaviour
 {
+    [Header("Sound")]
+    [SerializeField] private AudioClip _bgmSource;
+
     public GameManager GameManager { get; private set; }
     public DataManager DataManager { get; private set; }
+    public SoundManager SoundManager { get; private set; }
 
     public Camera MainCamera { get; private set; }
     public Transform CanvasTrans { get; private set; }
@@ -15,11 +19,17 @@ public class RunnerBase : MonoBehaviour
     {
         GameManager = GameManager.Instance;
         DataManager = DataManager.Instance;
+        SoundManager = SoundManager.Instance;
         MainCamera = Camera.main;
 
         if(_canvasBase == null)
         {
             _canvasBase = FindAnyObjectByType<CanvasBase>();
+        }
+
+        if(_bgmSource != null)
+        {
+            SoundManager.PlayBGM(_bgmSource);
         }
     }
 }
